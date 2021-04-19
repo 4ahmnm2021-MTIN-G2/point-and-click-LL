bc51e32c153f5aa371604f50dfbbd9a19df0b6c0
l.liebisch <letitia.liebisch03@htl-salzburg.ac.at>
2021-04-19 08:33:05 +0200

    InteractableObject

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,13 +6,20 @@ public class Player : MonoBehaviour
 {
     public Vector3 mousePos;
     public Vector3 mouseCameraPos;
+    public float speed = 0.1f;
+    public Vector3 playerPos;
 
     void Update()
     {
         if (Input.GetMouseButton(0))  {
             mousePos = Input.mousePosition;
             mouseCameraPos = Camera.main.ScreenToWorldPoint(mousePos);
-            this.transform.position = mouseCameraPos;
+            
+
+            playerPos =  new Vector3(mouseCameraPos.x, mouseCameraPos.y, this.transform.position.z);
+
         }
+
+        this.transform.position = Vector3.MoveTowards(this.transform.position, playerPos, speed);
     }
 }
