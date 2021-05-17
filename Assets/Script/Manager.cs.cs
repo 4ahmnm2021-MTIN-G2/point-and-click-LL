7a15919f40325c8b48ce14286ecd13f744743f72
l.liebisch <letitia.liebisch03@htl-salzburg.ac.at>
2021-05-17 08:44:53 +0200

    Manager script

---
+++ Assets/Script/Manager.cs.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+public bool _con1 = false;
+public bool con1{
+        get
+        {
+            return _con1;
+        } 
+        set
+        {
+            _con1 = value; 
+            Checkcon();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Checkcon(){
+        if (_con1 == true){
+            
+        }
+    }
+    
+}
+
